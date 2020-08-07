d74611f0c074a8cf6d2490a828f38f59db220c97
Yaser Afshar <ya.afshar@gmail.com>
2020-08-07 14:04:16 -0500

    Creating the user defined summation for small accumulator

--- src/myxsum.hpp
+++ src/myxsum.hpp
@@ -1,27 +1,33 @@
 #ifndef MYXSUM_HPP
 #define MYXSUM_HPP
 
-#include "xsum.hpp"
-
 #include <mpi.h>
 
-void myXSUM(void *invec, void *inoutvec, int *len, MPI_Datatype *datatype)
-{
-    double *in = static_cast<double *>(invec);
-    double *inout = static_cast<double *>(inoutvec);
+#include "xsum.hpp"
 
-    for (int i = 0; i < *len; ++i, ++in, ++inout)
-    {
-        xsum_large lacc;
-        lacc.add(*in);
-        lacc.add(*inout);
-        *inout = static_cast<double>(lacc.round());
+void create_small_accumulator_mpi_type(MPI_Datatype &small_accumulator_type) {
+    int const lengths[4] = {XSUM_SCHUNKS, 1, 1, 1};
+    MPI_Aint const displacements[4] = {0,
+                                       sizeof(xsum_schunk) * XSUM_SCHUNKS,
+                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 1),
+                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 2)};
+    MPI_Datatype types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT};
+
+    MPI_Type_create_struct(4, lengths, displacements, types, &small_accumulator_type);
+    MPI_Type_commit(&small_accumulator_type);
+}
+
+void destroy_small_accumulator_mpi_type(MPI_Datatype &small_accumulator_type) {
+    MPI_Type_free(&small_accumulator_type);
+}
+
+void myXSUM(void *invec, void *inoutvec, int *len, MPI_Datatype *datatype) {
+    xsum_small_accumulator *in = static_cast<xsum_small_accumulator *>(invec);
+    xsum_small_accumulator *inout = static_cast<xsum_small_accumulator *>(inoutvec);
+
+    for (int i = 0; i < *len; ++i, ++in, ++inout) {
+        xsum_small_add(inout, in);
     }
 }
 
-#endif // MYXSUM_HPP
-
-// MPI_Op XSUM;
-
-// /* create the XSUM user-op */
-// MPI_Op_create(myXSUM, true, &XSUM);
+#endif  // MYXSUM_HPP
