903c663e33c7de571cffbe580b657b684c223f27
Yaser Afshar <ya.afshar@gmail.com>
2020-08-13 10:48:58 -0500

    Correct the comments

--- examples/mpi_simple.cpp
+++ examples/mpi_simple.cpp
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2019--2020, Regents of the University of Minnesota.
+// All rights reserved.
+//
+// Contributors:
+//    Yaser Afshar
+//
+
 #include <mpi.h>
 
 #include <iomanip>
@@ -18,9 +26,10 @@ int main() {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+    // Create the MPI data type of the superaccumulator
     MPI_Datatype acc_mpi = create_mpi_type<xsum_large_accumulator>();
 
-    /* Create the XSUM user-op */
+    // Create the XSUM user-op
     MPI_Op XSUM = create_XSUM<xsum_large_accumulator>();
 
     double const a = 0.239e-3;
@@ -43,9 +52,10 @@ int main() {
                   << ", sum 2 =  " << std::setprecision(20) << xsum_large_round(&lacc) << std::endl;
     }
 
-    /* Free the created user-op */
+    // Free the created user-op
     destroy_XSUM(XSUM);
 
+    // Free the created MPI data type
     destroy_mpi_type(acc_mpi);
 
     // Finalize the MPI environment.
