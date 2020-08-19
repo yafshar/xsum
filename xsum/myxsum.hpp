469833156504ef518d2a38ef03fcbb2f0f8ba3b5
Yaser Afshar <ya.afshar@gmail.com>
2020-08-19 11:30:29 -0500

    Update the indentation and format style

--- xsum/myxsum.hpp
+++ xsum/myxsum.hpp
@@ -42,8 +42,8 @@ namespace {
 /*!
  * \brief Create a mpi type object
  *
- * \tparam T data type one of \c xsum_small_accumulator or \c xsum_large_accumulator
- * \param datatype
+ * \tparam T data type one of \c xsum_small_accumulator or \c
+ * xsum_large_accumulator \param datatype
  */
 template <typename T>
 void create_mpi_type(MPI_Datatype &datatype);
@@ -51,8 +51,8 @@ void create_mpi_type(MPI_Datatype &datatype);
 /*!
  * \brief Create a mpi type object
  *
- * \tparam T data type one of \c xsum_small_accumulator or \c xsum_large_accumulator
- * \return MPI_Datatype
+ * \tparam T data type one of \c xsum_small_accumulator or \c
+ * xsum_large_accumulator \return MPI_Datatype
  */
 template <typename T>
 MPI_Datatype create_mpi_type();
@@ -68,14 +68,14 @@ void destroy_mpi_type(MPI_Datatype &datatype);
  * \brief A user-defined xsum function
  *
  * This is a user-defined global xsum operation on \c xsum_small_accumulator or
- * \c xsum_large_accumulator type to an op handle that can subsequently be used in
- * \c MPI_Reduce, \c MPI_Allreduce, \c MPI_Reduce_scatter, and \c MPI_Scan.
+ * \c xsum_large_accumulator type to an op handle that can subsequently be used
+ * in \c MPI_Reduce, \c MPI_Allreduce, \c MPI_Reduce_scatter, and \c MPI_Scan.
  *
- * \tparam T data type one of \c xsum_small_accumulator or \c xsum_large_accumulator
- * \param invec arrays of len elements that \c myXSUM function is combining.
- * \param inoutvec arrays of len elements that \c myXSUM function is combining.
- * \param len length
- * \param datatype a handle to the data type that was passed into the call
+ * \tparam T data type one of \c xsum_small_accumulator or \c
+ * xsum_large_accumulator \param invec arrays of len elements that \c myXSUM
+ * function is combining. \param inoutvec arrays of len elements that \c myXSUM
+ * function is combining. \param len length \param datatype a handle to the data
+ * type that was passed into the call
  */
 template <typename T>
 void myXSUM(void *invec, void *inoutvec, int *len, MPI_Datatype *datatype);
@@ -95,152 +95,164 @@ void destroy_XSUM(MPI_Op &SSUM);
 
 template <typename T>
 void create_mpi_type(MPI_Datatype &datatype) {
-    std::cerr << "Not implemented on purpose!" << std::endl;
-    int ierr;
-    MPI_Abort(MPI_COMM_WORLD, ierr);
+  std::cerr << "Not implemented on purpose!" << std::endl;
+  int ierr;
+  MPI_Abort(MPI_COMM_WORLD, ierr);
 }
 
 template <>
-void create_mpi_type<xsum_small_accumulator>(MPI_Datatype &small_accumulator_type) {
-    int const lengths[4] = {XSUM_SCHUNKS, 1, 1, 1};
-    MPI_Aint const displacements[4] = {0,
-                                       sizeof(xsum_schunk) * XSUM_SCHUNKS,
-                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 1),
-                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 2)};
-    MPI_Datatype const types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT};
-    MPI_Type_create_struct(4, lengths, displacements, types, &small_accumulator_type);
-    MPI_Type_commit(&small_accumulator_type);
+void create_mpi_type<xsum_small_accumulator>(
+    MPI_Datatype &small_accumulator_type) {
+  int const lengths[4] = {XSUM_SCHUNKS, 1, 1, 1};
+  MPI_Aint const displacements[4] = {0, sizeof(xsum_schunk) * XSUM_SCHUNKS,
+                                     sizeof(xsum_schunk) * (XSUM_SCHUNKS + 1),
+                                     sizeof(xsum_schunk) * (XSUM_SCHUNKS + 2)};
+  MPI_Datatype const types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T,
+                                 MPI_INT};
+  MPI_Type_create_struct(4, lengths, displacements, types,
+                         &small_accumulator_type);
+  MPI_Type_commit(&small_accumulator_type);
 }
 
 template <>
-void create_mpi_type<xsum_large_accumulator>(MPI_Datatype &large_accumulator_type) {
-    int const lengths[8] = {XSUM_LCHUNKS, XSUM_LCHUNKS, XSUM_LCHUNKS / 64, 1, XSUM_SCHUNKS, 1, 1, 1};
-    MPI_Aint const d1 = sizeof(xsum_lchunk) * XSUM_LCHUNKS;
-    MPI_Aint const d2 = d1 + sizeof(xsum_lcount) * XSUM_LCHUNKS;
-    MPI_Aint const d3 = d2 + sizeof(xsum_used) * XSUM_LCHUNKS / 64;
-    MPI_Aint const d4 = d3 + sizeof(xsum_used);
-    MPI_Aint const d5 = d4 + sizeof(xsum_schunk) * XSUM_SCHUNKS;
-    MPI_Aint const d6 = d5 + sizeof(xsum_schunk);
-    MPI_Aint const d7 = d6 + sizeof(xsum_schunk);
-    MPI_Aint const displacements[8] = {0, d1, d2, d3, d4, d5, d6, d7};
-    MPI_Datatype const types[8] = {MPI_INT64_T, MPI_INT16_T, MPI_UINT64_T, MPI_UINT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT};
-    MPI_Type_create_struct(8, lengths, displacements, types, &large_accumulator_type);
-    MPI_Type_commit(&large_accumulator_type);
+void create_mpi_type<xsum_large_accumulator>(
+    MPI_Datatype &large_accumulator_type) {
+  int const lengths[8] = {
+      XSUM_LCHUNKS, XSUM_LCHUNKS, XSUM_LCHUNKS / 64, 1, XSUM_SCHUNKS, 1, 1, 1};
+  MPI_Aint const d1 = sizeof(xsum_lchunk) * XSUM_LCHUNKS;
+  MPI_Aint const d2 = d1 + sizeof(xsum_lcount) * XSUM_LCHUNKS;
+  MPI_Aint const d3 = d2 + sizeof(xsum_used) * XSUM_LCHUNKS / 64;
+  MPI_Aint const d4 = d3 + sizeof(xsum_used);
+  MPI_Aint const d5 = d4 + sizeof(xsum_schunk) * XSUM_SCHUNKS;
+  MPI_Aint const d6 = d5 + sizeof(xsum_schunk);
+  MPI_Aint const d7 = d6 + sizeof(xsum_schunk);
+  MPI_Aint const displacements[8] = {0, d1, d2, d3, d4, d5, d6, d7};
+  MPI_Datatype const types[8] = {MPI_INT64_T,  MPI_INT16_T, MPI_UINT64_T,
+                                 MPI_UINT64_T, MPI_INT64_T, MPI_INT64_T,
+                                 MPI_INT64_T,  MPI_INT};
+  MPI_Type_create_struct(8, lengths, displacements, types,
+                         &large_accumulator_type);
+  MPI_Type_commit(&large_accumulator_type);
 }
 
 template <typename T>
 MPI_Datatype create_mpi_type() {
-    std::cerr << "Not implemented on purpose!" << std::endl;
-    int ierr;
-    MPI_Abort(MPI_COMM_WORLD, ierr);
-    return MPI_DATATYPE_NULL;
+  std::cerr << "Not implemented on purpose!" << std::endl;
+  int ierr;
+  MPI_Abort(MPI_COMM_WORLD, ierr);
+  return MPI_DATATYPE_NULL;
 }
 
 template <>
 MPI_Datatype create_mpi_type<xsum_small_accumulator>() {
-    MPI_Datatype small_accumulator_type;
-    int const lengths[4] = {XSUM_SCHUNKS, 1, 1, 1};
-    MPI_Aint const displacements[4] = {0,
-                                       sizeof(xsum_schunk) * XSUM_SCHUNKS,
-                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 1),
-                                       sizeof(xsum_schunk) * (XSUM_SCHUNKS + 2)};
-    MPI_Datatype const types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT};
-    MPI_Type_create_struct(4, lengths, displacements, types, &small_accumulator_type);
-    MPI_Type_commit(&small_accumulator_type);
-    return small_accumulator_type;
+  MPI_Datatype small_accumulator_type;
+  int const lengths[4] = {XSUM_SCHUNKS, 1, 1, 1};
+  MPI_Aint const displacements[4] = {0, sizeof(xsum_schunk) * XSUM_SCHUNKS,
+                                     sizeof(xsum_schunk) * (XSUM_SCHUNKS + 1),
+                                     sizeof(xsum_schunk) * (XSUM_SCHUNKS + 2)};
+  MPI_Datatype const types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T,
+                                 MPI_INT};
+  MPI_Type_create_struct(4, lengths, displacements, types,
+                         &small_accumulator_type);
+  MPI_Type_commit(&small_accumulator_type);
+  return small_accumulator_type;
 }
 
 template <>
 MPI_Datatype create_mpi_type<xsum_large_accumulator>() {
-    MPI_Datatype large_accumulator_type;
-    int const lengths[8] = {XSUM_LCHUNKS, XSUM_LCHUNKS, XSUM_LCHUNKS / 64, 1, XSUM_SCHUNKS, 1, 1, 1};
-    MPI_Aint const d1 = sizeof(xsum_lchunk) * XSUM_LCHUNKS;
-    MPI_Aint const d2 = d1 + sizeof(xsum_lcount) * XSUM_LCHUNKS;
-    MPI_Aint const d3 = d2 + sizeof(xsum_used) * XSUM_LCHUNKS / 64;
-    MPI_Aint const d4 = d3 + sizeof(xsum_used);
-    MPI_Aint const d5 = d4 + sizeof(xsum_schunk) * XSUM_SCHUNKS;
-    MPI_Aint const d6 = d5 + sizeof(xsum_schunk);
-    MPI_Aint const d7 = d6 + sizeof(xsum_schunk);
-    MPI_Aint const displacements[8] = {0, d1, d2, d3, d4, d5, d6, d7};
-    MPI_Datatype const types[8] = {MPI_INT64_T, MPI_INT16_T, MPI_UINT64_T, MPI_UINT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT};
-    MPI_Type_create_struct(8, lengths, displacements, types, &large_accumulator_type);
-    MPI_Type_commit(&large_accumulator_type);
-    return large_accumulator_type;
+  MPI_Datatype large_accumulator_type;
+  int const lengths[8] = {
+      XSUM_LCHUNKS, XSUM_LCHUNKS, XSUM_LCHUNKS / 64, 1, XSUM_SCHUNKS, 1, 1, 1};
+  MPI_Aint const d1 = sizeof(xsum_lchunk) * XSUM_LCHUNKS;
+  MPI_Aint const d2 = d1 + sizeof(xsum_lcount) * XSUM_LCHUNKS;
+  MPI_Aint const d3 = d2 + sizeof(xsum_used) * XSUM_LCHUNKS / 64;
+  MPI_Aint const d4 = d3 + sizeof(xsum_used);
+  MPI_Aint const d5 = d4 + sizeof(xsum_schunk) * XSUM_SCHUNKS;
+  MPI_Aint const d6 = d5 + sizeof(xsum_schunk);
+  MPI_Aint const d7 = d6 + sizeof(xsum_schunk);
+  MPI_Aint const displacements[8] = {0, d1, d2, d3, d4, d5, d6, d7};
+  MPI_Datatype const types[8] = {MPI_INT64_T,  MPI_INT16_T, MPI_UINT64_T,
+                                 MPI_UINT64_T, MPI_INT64_T, MPI_INT64_T,
+                                 MPI_INT64_T,  MPI_INT};
+  MPI_Type_create_struct(8, lengths, displacements, types,
+                         &large_accumulator_type);
+  MPI_Type_commit(&large_accumulator_type);
+  return large_accumulator_type;
 }
 
-void destroy_mpi_type(MPI_Datatype &user_type) {
-    MPI_Type_free(&user_type);
-}
+void destroy_mpi_type(MPI_Datatype &user_type) { MPI_Type_free(&user_type); }
 
 template <typename T>
 void myXSUM(void *invec, void *inoutvec, int *len, MPI_Datatype *datatype) {
-    std::cerr << "Not implemented on purpose!" << std::endl;
-    int ierr;
-    MPI_Abort(MPI_COMM_WORLD, ierr);
+  std::cerr << "Not implemented on purpose!" << std::endl;
+  int ierr;
+  MPI_Abort(MPI_COMM_WORLD, ierr);
 }
 
 template <>
-void myXSUM<xsum_small_accumulator>(void *invec, void *inoutvec, int *len, MPI_Datatype * /* datatype*/) {
-    xsum_small_accumulator *in = static_cast<xsum_small_accumulator *>(invec);
-    xsum_small_accumulator *inout = static_cast<xsum_small_accumulator *>(inoutvec);
+void myXSUM<xsum_small_accumulator>(void *invec, void *inoutvec, int *len,
+                                    MPI_Datatype * /* datatype*/) {
+  xsum_small_accumulator *in = static_cast<xsum_small_accumulator *>(invec);
+  xsum_small_accumulator *inout =
+      static_cast<xsum_small_accumulator *>(inoutvec);
 
-    for (int i = 0; i < *len; ++i, ++in, ++inout) {
-        xsum_add(inout, in);
-    }
+  for (int i = 0; i < *len; ++i, ++in, ++inout) {
+    xsum_add(inout, in);
+  }
 }
 
 template <>
-void myXSUM<xsum_large_accumulator>(void *invec, void *inoutvec, int *len, MPI_Datatype * /* datatype*/) {
-    xsum_large_accumulator *in = static_cast<xsum_large_accumulator *>(invec);
-    xsum_large_accumulator *inout = static_cast<xsum_large_accumulator *>(inoutvec);
+void myXSUM<xsum_large_accumulator>(void *invec, void *inoutvec, int *len,
+                                    MPI_Datatype * /* datatype*/) {
+  xsum_large_accumulator *in = static_cast<xsum_large_accumulator *>(invec);
+  xsum_large_accumulator *inout =
+      static_cast<xsum_large_accumulator *>(inoutvec);
 
-    for (int i = 0; i < *len; ++i, ++in, ++inout) {
-        xsum_add(inout, in);
-    }
+  for (int i = 0; i < *len; ++i, ++in, ++inout) {
+    xsum_add(inout, in);
+  }
 }
 
 template <typename T>
 void create_XSUM(MPI_Op &XSUM) {
-    std::cerr << "Not implemented on purpose!" << std::endl;
-    int ierr;
-    MPI_Abort(MPI_COMM_WORLD, ierr);
+  std::cerr << "Not implemented on purpose!" << std::endl;
+  int ierr;
+  MPI_Abort(MPI_COMM_WORLD, ierr);
 }
 
 template <>
 void create_XSUM<xsum_small_accumulator>(MPI_Op &XSUM) {
-    MPI_Op_create(&myXSUM<xsum_small_accumulator>, true, &XSUM);
+  MPI_Op_create(&myXSUM<xsum_small_accumulator>, true, &XSUM);
 }
 
 template <>
 void create_XSUM<xsum_large_accumulator>(MPI_Op &XSUM) {
-    MPI_Op_create(&myXSUM<xsum_large_accumulator>, true, &XSUM);
+  MPI_Op_create(&myXSUM<xsum_large_accumulator>, true, &XSUM);
 }
 
 template <typename T>
 MPI_Op create_XSUM() {
-    std::cerr << "Not implemented on purpose!" << std::endl;
-    int ierr;
-    MPI_Abort(MPI_COMM_WORLD, ierr);
-    return MPI_OP_NULL;
+  std::cerr << "Not implemented on purpose!" << std::endl;
+  int ierr;
+  MPI_Abort(MPI_COMM_WORLD, ierr);
+  return MPI_OP_NULL;
 }
 
 template <>
 MPI_Op create_XSUM<xsum_small_accumulator>() {
-    MPI_Op XSUM;
-    MPI_Op_create(&myXSUM<xsum_small_accumulator>, true, &XSUM);
-    return XSUM;
+  MPI_Op XSUM;
+  MPI_Op_create(&myXSUM<xsum_small_accumulator>, true, &XSUM);
+  return XSUM;
 }
 
 template <>
 MPI_Op create_XSUM<xsum_large_accumulator>() {
-    MPI_Op XSUM;
-    MPI_Op_create(&myXSUM<xsum_large_accumulator>, true, &XSUM);
-    return XSUM;
+  MPI_Op XSUM;
+  MPI_Op_create(&myXSUM<xsum_large_accumulator>, true, &XSUM);
+  return XSUM;
 }
 
-void destroy_XSUM(MPI_Op &XSUM) {
-    MPI_Op_free(&XSUM);
-}
+void destroy_XSUM(MPI_Op &XSUM) { MPI_Op_free(&XSUM); }
 }  // namespace
 
 #endif  // MYXSUM_HPP
